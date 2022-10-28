50fbb400b989d1551f167f8c66e4361a95fa9908
Pregonas <47900248+Pregonas@users.noreply.github.com>
2022-10-27 21:44:28 -0500

    Update telegram.py

--- code/telegram.py
+++ code/telegram.py
@@ -18,7 +18,7 @@ chats_id:
 """
 
 def telegram_bot_sendtext(bot_message, bot_chatID):
-    bot_token = '1258791685:AAEJdT8SUkseRehqBY5RkglWNQFkMKp63WU'
+    bot_token = 'Your_Bot_Token'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
 
